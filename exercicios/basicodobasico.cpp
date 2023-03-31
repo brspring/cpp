33ef258b1f8680f2f148fb6f2239aafdbdfff73c
bruno <brunoazizsm@gmail.com>
2023-03-31 17:28:56 -0300

    aaa

---
+++ exercicios/basicodobasico.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a, b;
+    string x;
+
+    ios::sync_with_stdio(0);
+    cin.tie(0); //otimização da entrada e saida
+
+    cin >> a >> b >> x;
+
+    cout << a + b << " " << x;
+}
